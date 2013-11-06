89d9e4a5b527d0aab3540937ef330d7d6893ae52
brian sorahan <bsorahan@gmail.com>
2013-11-06 09:18:00 -0600

    more rtti examples

--- ch15/rtti2.cpp
+++ ch15/rtti2.cpp
@@ -4,5 +4,26 @@
 #include <ctime>
 #include <typeinfo>
 
+#include "rtti.hpp"
+
 using namespace std;
 
+int main(int argc, char ** argv) {
+    srand(time(0));
+    Grand * pg;
+    Superb * ps;
+    for (int i = 0; i < 5; i++) {
+        pg = GetOne();
+        cout << "Now processing type " << typeid(*pg).name() << ".\n";
+        pg->Speak();
+        if (ps = dynamic_cast<Superb *>(pg)) {
+            ps->Say();
+        }
+
+        if (typeid(Magnificent) == typeid(*pg)) {
+            cout << "Yes, you're really magnificent!\n";
+        }
+    }
+
+    return 0;
+}
