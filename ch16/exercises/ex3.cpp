893b9f1f86163dc0f28ede504d3529c0387992fd
brian sorahan <bsorahan@gmail.com>
2013-11-28 10:47:34 -0600

    ch16 exercises

---
+++ ch16/exercises/ex3.cpp
@@ -0,0 +1,25 @@
+// hangman game using string methods
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+
+using std::string;
+using std::vector;
+
+const int NUM = 26;
+vector<string> wordlist;
+
+bool readwords(vector<string> & ws);
+
+int main(int argc, char ** argv) {
+    return 0;
+}
+
+bool readwords(vector<string> & ws) {
+    return true;
+}
